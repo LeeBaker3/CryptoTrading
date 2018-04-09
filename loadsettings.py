dd169d2608d4c5d48c6a37a308c31fd40d5eb3e2
Lee Baker <lee@MacBook-Pro.mshome.net>
2018-04-09 22:31:27 +1000

    loadsettings.py - get_Settings method modified to import from a json file

--- loadsettings.py
+++ loadsettings.py
@@ -1,9 +1,15 @@
 class LoadSetting(object):
 
+    import json
+
     def __init__(self, settings = dict()):
         self.settings = settings
     
 
     def get_Settings(self):
+
+        with open("settings.json") as json_file:
+             json_data = self.json.load(json_file)
+             self.settings = json_data[0]
         return self.settings
         
