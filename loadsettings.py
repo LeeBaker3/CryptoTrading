ab2b710be0eeab250ef9769c3bcff9d9170a85f1
Lee Baker <lee@MacBook-Pro.local>
2018-04-10 12:38:10 +1000

    Added print of settings to console

--- loadsettings.py
+++ loadsettings.py
@@ -5,11 +5,10 @@ class LoadSetting(object):
     def __init__(self, settings = dict()):
         self.settings = settings
     
-
     def get_Settings(self):
 
         with open("settings.json") as json_file:
-             json_data = self.json.load(json_file)
-             self.settings = json_data[0]
+             self.settings = self.json.load(json_file)
+
         return self.settings
         
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from LoadSettings import LoadSetting as ld
+#import tensorflow as tf
+from loadsettings import LoadSetting as ld
 
 #Load applications settings
 
 s = ld()
 settings = s.get_Settings()
+print('Mode: ', settings['MODE'])
