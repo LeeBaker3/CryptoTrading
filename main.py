203a024a57ab73bb9e4c8dbeea54f3aa93ce1898
Lee Baker <lee@MacBook-Pro.local>
2018-04-10 23:59:12 +1000

    Started adding changes to use an XML settings file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-#import tensorflow as tf
+import tensorflow as tf
 from loadsettings import LoadSetting as ld
 
-#Load applications settings
-
+# Load applications settings
+# Vadriable 's' is a LoadSetting object
+# Variable 'settings' is dictonary that holds applications settings
+# Application settings definition xxxxxx 
 s = ld()
 settings = s.get_Settings()
 print('Mode: ', settings['MODE'])
